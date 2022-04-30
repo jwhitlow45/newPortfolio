407d645abff0a1a5ecaa0bc0137732b2fd98dbec
j <j@pop-os.localdomain>
2022-04-30 00:18:28 -0700

    feat:add event listeners for navbar and load home on site load

---
+++ js/navbar.js
@@ -0,0 +1,31 @@
+//global elements
+const main_content = document.getElementById("main-content");
+
+// navbar buttons
+const navbar_home = document.getElementById("home");
+const navbar_education = document.getElementById("education");
+const navbar_experience = document.getElementById("experience");
+const navbar_projects = document.getElementById("projects");
+const navbar_references = document.getElementById("references");
+
+// Event listeners
+navbar_home.addEventListener("click", changePageContent);
+navbar_education.addEventListener("click", changePageContent);
+navbar_experience.addEventListener("click", changePageContent);
+navbar_projects.addEventListener("click", changePageContent);
+navbar_references.addEventListener("click", changePageContent);
+
+function clearPageContent() {
+  main_content.innerHTML = "";
+}
+
+async function changePageContent(event) {
+  clearPageContent();
+  page_path = "./html/" + event.target.id + ".html";
+  main_content.innerHTML=`<object type="text/html" data="${page_path}" ></object>`;
+}
+
+function init() {
+  clearPageContent();
+  main_content.innerHTML=`<object type="text/html" data="./html/home.html" ></object>`;
+}
